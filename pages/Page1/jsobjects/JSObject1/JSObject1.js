c59769ac8725c9afff502a636968167e1c5be1bc
SebasMena28 <sebasmena28>
2024-04-02 17:18:01 +0000

    mejora del formulario

--- pages/Page1/jsobjects/JSObject1/JSObject1.js
+++ pages/Page1/jsobjects/JSObject1/JSObject1.js
@@ -2,37 +2,153 @@ export default {
 	myVar1: [],
 	myVar2: {},
 	myFun1 () {
-		var direccion = Select1.selectedOptionValue;
-var subgrupo =  Select5.selectedOptionValue;
-var CPC =  Select6.selectedOptionValue;
-var detalle =  Input1.text;
-var fondoBID =  Select7.selectedOptionValue;
-var tipo_compra =  Select8.selectedOptionValue;
-var tipo_producto =  Select8Copy1.selectedOptionValue;
-var cantidad_anual =  Input2.text;
-var catalogo_electronico =  Select8Copy2.selectedOptionValue;
-var unidad =  Select8Copy.selectedOptionValue;
-var tipo_regimen =  Select8CopyCopy.selectedOptionValue;
-var costo_unitario =  Input2Copy.text;
-var tipo_presupuesto =  Select8CopyCopy.selectedOptionValue;
-var codigo_prestamo_BID =  Input2Copy1.text;
-var codigo_proyecto_BID =  Input2Copy1Copy.text;
-var procedimiento_sugerido =  Select10.selectedOptionValue;
-
-// Realizar validaciones
-if (direccion === "" || subgrupo === "" || CPC === "" || detalle === "" || fondoBID === "" || tipo_compra === "" || tipo_producto === "" || cantidad_anual === "" || catalogo_electronico === "" || unidad === "" || tipo_regimen === "" || costo_unitario === "" || tipo_presupuesto === "" || codigo_prestamo_BID === "" || codigo_proyecto_BID === "" || procedimiento_sugerido === "") {
-    // Si algún campo está vacío, muestra un mensaje de error
-    showAlert({
-        title: "Error",
-        message: "Por favor, complete todos los campos del formulario.",
-        severity: "error"
-    });
-} else {
-    Api2.run().then(() => {
-  showModal('Modal1');
-});
-}
+		
 	},
+	
+	
+	obtenerNumeroPP(texto){
+		let partes = texto.split(' - ')
+		let numero = partes[0];
+		return numero;
+	},
+	
+	obtenerTextoPP(texto){
+		let partes = texto.split(' - ')
+		let descripcion = partes[1];
+		return descripcion;
+	},
+	
+	obtenerNumeroCPC(texto){
+		let partes = texto.split('-')
+		let numero = partes[0];
+		return numero;
+	},
+	
+	obtenerTextoCPC(texto){
+		let partes = texto.split('-')
+		let descripcion = partes[1];
+		return descripcion;
+	},
+	
+	obtenerMonto(cantidad, dinero){
+		return cantidad*dinero
+	},
+	
+	obtenerCuatrimestre(valor){
+		if (valor) return "S"
+		else return ""
+	},
+	
+	obtenerPeriodo(c1, c2, c3){
+		let cadena = ""
+		if (c1) cadena += "CUATRIMESTRE1"
+		if (c2) cadena += "CUATRIMESTRE2"
+		if (c3) cadena += "CUATRIMESTRE3"
+		
+		return cadena
+	},
+	
+	obteneranio(){
+		const fecha = new Date();
+    return fecha.getFullYear();
+	},
+	
+	obtenerCodigoSERCOP(){
+		return "";
+	},
+	
+	obtenerFeriaInclusiva(){
+		return "";
+	},
+	
+	obtenerReforma(){
+		return "NO"
+	},
+	
+	obtenerProcesoSugerido(tipoCompra, tipoProducto, catalogoElectronico, costoUnitario, cantidad, fondoBID){
+		let monto = costoUnitario * cantidad;
+		let procesoSugerido = "MENOR CUANTIA"
+		
+		switch (fondoBID){
+      case "SI":
+        switch (tipoCompra){
+          case "BIEN":
+            if(tipoProducto == "NO NORMALIZADO"){
+              if (monto<66593.64) procesoSugerido = "MENOR CUANTIA"
+              else procesoSugerido = "COTIZACION"
+            }
+            else{
+              procesoSugerido = "SUBASTA INVERSA ELECTRONICA"
+            }
+          break;
+          case "SERVICIO":
+            if(tipoProducto == "NO NORMALIZADO"){
+              if (monto<66593.64) procesoSugerido = "MENOR CUANTIA"
+              else procesoSugerido = "COTIZACION"
+            }
+            else{
+              procesoSugerido = "SUBASTA INVERSA ELECTRONICA"
+            }
+          break;
+          case "OBRAS":
+            if (monto < 233077.74) procesoSugerido = "MENOR CUANTIA"
+            else if (monto > 998904.59) procesoSugerido = "LICITACION"
+            else procesoSugerido = "COTIZACION"
+          break;
+          case "CONSULTORIA":
+            if (monto <= 66593.64) procesoSugerido = "CONTRATACION DIRECTA"
+            else procesoSugerido = "LISTA CORTA"
+          break;
+        }
+      break
+      case "NO":
+        switch (tipoCompra){
+          case "CONSULTORIA":
+            if (monto <= 66593.64) procesoSugerido = "CONTRATACION DIRECTA"
+            if (monto >= 499452.30) procesoSugerido = "CONCURSO PUBLICO"
+            else procesoSugerido = "LISTA CORTA"
+          break;
+          case "OBRAS":
+            if (monto < 233077.74) procesoSugerido = "MENOR CUANTIA"
+            else if (monto > 998904.59) procesoSugerido = "LICITACION"
+            else procesoSugerido = "COTIZACION"
+          break;
+          case "BIEN":
+          if(tipoProducto == "NORMALIZADO"){
+            if(catalogoElectronico == "SI") procesoSugerido = "COMPRA POR CATALOGO ELECTRONICO"
+            else{
+              if (monto <= 6659.36) procesoSugerido = "INFIMA CUANTIA"
+              else procesoSugerido = "SUBASTA INVERSA ELECTRONICA"
+            }
+          }
+          else{
+            if(monto < 66593.64) procesoSugerido = "MENOR CUANTIA"
+            else if (monto > 499452.30) procesoSugerido = "LICITACION"
+            else procesoSugerido = "COTIZACION"
+          }
+            
+          break;
+          case "SERVICIO":
+          if(tipoProducto == "NORMALIZADO"){
+            if(catalogoElectronico == "SI") procesoSugerido = "COMPRA POR CATALOGO ELECTRONICO"
+            else{
+              if (monto <= 6659.36) procesoSugerido = "INFIMA CUANTIA"
+              else procesoSugerido = "SUBASTA INVERSA ELECTRONICA"
+            }
+          }
+          else{
+            if(monto < 66593.64) procesoSugerido = "MENOR CUANTIA"
+            else if (monto > 499452.30) procesoSugerido = "LICITACION"
+            else procesoSugerido = "COTIZACION"
+          }
+            
+          break
+        }
+      break
+    }
+    return procesoSugerido;
+	},
+	
 	async myFun2 () {
 		//	use async-await or promises
 		//	await storeValue('varName', 'hello world')
